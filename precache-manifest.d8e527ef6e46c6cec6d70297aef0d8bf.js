18a6a3dbdabedf86dc1494e36c7ec15c44b09777
Sphinx7777 <spamoglot13@gmail.com>
2019-09-14 18:51:35 +0300

    Updates

---
+++ precache-manifest.d8e527ef6e46c6cec6d70297aef0d8bf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a34ba72042d73f12d485572eaa246b",
+    "url": "/Sphinx7777/Portfolio/index.html"
+  },
+  {
+    "revision": "4f4dd59df415b6bcb27d",
+    "url": "/Sphinx7777/Portfolio/static/css/main.79cf8ad9.chunk.css"
+  },
+  {
+    "revision": "02b512e17c4880b3a27e",
+    "url": "/Sphinx7777/Portfolio/static/js/2.f59fa08d.chunk.js"
+  },
+  {
+    "revision": "4f4dd59df415b6bcb27d",
+    "url": "/Sphinx7777/Portfolio/static/js/main.1d96802b.chunk.js"
+  },
+  {
+    "revision": "3b28480b025bdaa938b3",
+    "url": "/Sphinx7777/Portfolio/static/js/runtime~main.36c5eda1.js"
+  },
+  {
+    "revision": "de627dcbb2f272bee2ce188eb42e1620",
+    "url": "/Sphinx7777/Portfolio/static/media/Settings.de627dcb.ico"
+  },
+  {
+    "revision": "8234a0304c9bd3ac8cbee9f91f723e9f",
+    "url": "/Sphinx7777/Portfolio/static/media/add.8234a030.ico"
+  },
+  {
+    "revision": "2bcad980cf95fcdfb7337205ea8612d5",
+    "url": "/Sphinx7777/Portfolio/static/media/git.2bcad980.ico"
+  },
+  {
+    "revision": "b4166a32ba33162329d4e62f2ecbed69",
+    "url": "/Sphinx7777/Portfolio/static/media/lamp.b4166a32.ico"
+  },
+  {
+    "revision": "7046d3c316edf26478a2018c8d219f91",
+    "url": "/Sphinx7777/Portfolio/static/media/search3.7046d3c3.ico"
+  },
+  {
+    "revision": "5e18d5208289f562929dd840976ccba3",
+    "url": "/Sphinx7777/Portfolio/static/media/telegram.5e18d520.ico"
+  }
+]);
